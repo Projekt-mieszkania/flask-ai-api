7534ccc8cbde366548c24de5d5780e01916e65ee
Projekt-mieszkania <sedumdesign777@gmail.com>
2025-05-18 16:34:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ def generate():
     url = data.get("url")
 
     try:
-        response = requests.get(url, timeout=10)
+        headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/122 Safari/537.36"
+}
+        response = requests.get(url, headers=headers, timeout=10)
+
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # Название
